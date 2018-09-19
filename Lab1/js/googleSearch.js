4deb070de11c314a1e5d9387e13bec754580a57c
toadSTL <gregory.dylan.brown@gmail.com>
2018-09-18 23:17:30 -0500

    search from speech2text

--- Lab1/js/googleSearch.js
+++ Lab1/js/googleSearch.js
@@ -17,5 +17,67 @@ myApp.controller('myController', ['$scope', function($scope) {
         });
     }
 
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+    var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+    var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+    var words = [];
+    var grammar = '#JSGF V1.0; grammar words; public <word> = ' + words.join(' | ') + ' ;'
+
+    var recognition = new SpeechRecognition();
+    var speechRecognitionList = new SpeechGrammarList();
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = speechRecognitionList;
+//recognition.continuous = false;
+    recognition.lang = 'en-US';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    var diagnostic = document.querySelector('.output');
+    var bg = document.querySelector('html');
+    var hints = document.querySelector('.hints');
+
+//var colorHTML= '';
+    words.forEach(function(v, i, a){
+        console.log(v, i);
+    });
+    //hints.innerHTML = 'Tap/click then say a word to show the word.';
+
+    $scope.listen = function() {
+        recognition.start();
+        console.log('Ready to receive a word.');
+    }
+
+    recognition.onresult = function(event) {
+        // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+        // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+        // It has a getter so it can be accessed like an array
+        // The [last] returns the SpeechRecognitionResult at the last position.
+        // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+        // These also have getters so they can be accessed like arrays.
+        // The [0] returns the SpeechRecognitionAlternative at position 0.
+        // We then return the transcript property of the SpeechRecognitionAlternative object
+
+        var last = event.results.length - 1;
+        var word = event.results[last][0].transcript;
+
+        $scope.searchTerm = word;
+        diagnostic.textContent = 'Result received: ' + word + '.';
+        //bg.style.backgroundColor = color;
+        console.log('Confidence: ' + event.results[0][0].confidence);
+    }
+
+    recognition.onspeechend = function() {
+        recognition.stop();
+    }
+
+    recognition.onnomatch = function(event) {
+        diagnostic.textContent = "I didn't recognise that word.";
+    }
+
+    recognition.onerror = function(event) {
+        diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+    }
+
 
 }]);
--- Lab1/js/speech2Text.js
+++ Lab1/js/speech2Text.js
@@ -2,8 +2,8 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
-var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+var words = [];
+var grammar = '#JSGF V1.0; grammar words; public <word> = ' + words.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
@@ -18,16 +18,15 @@ var diagnostic = document.querySelector('.output');
 var bg = document.querySelector('html');
 var hints = document.querySelector('.hints');
 
-var colorHTML= '';
-colors.forEach(function(v, i, a){
+//var colorHTML= '';
+words.forEach(function(v, i, a){
     console.log(v, i);
-    colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
 });
-hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try '+ colorHTML + '.';
+hints.innerHTML = 'Tap/click then say a word to show the word.';
 
 document.body.onclick = function() {
     recognition.start();
-    console.log('Ready to receive a color command.');
+    console.log('Ready to receive a word.');
 }
 
 recognition.onresult = function(event) {
@@ -41,10 +40,10 @@ recognition.onresult = function(event) {
     // We then return the transcript property of the SpeechRecognitionAlternative object
 
     var last = event.results.length - 1;
-    var color = event.results[last][0].transcript;
+    var word = event.results[last][0].transcript;
 
-    diagnostic.textContent = 'Result received: ' + color + '.';
-    bg.style.backgroundColor = color;
+    diagnostic.textContent = 'Result received: ' + word + '.';
+    //bg.style.backgroundColor = color;
     console.log('Confidence: ' + event.results[0][0].confidence);
 }
 
@@ -53,7 +52,7 @@ recognition.onspeechend = function() {
 }
 
 recognition.onnomatch = function(event) {
-    diagnostic.textContent = "I didn't recognise that color.";
+    diagnostic.textContent = "I didn't recognise that word.";
 }
 
 recognition.onerror = function(event) {
