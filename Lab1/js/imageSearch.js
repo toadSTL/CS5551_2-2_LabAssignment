6737adeacc269b4c20b3a8a011b6690f34372f4e
toadSTL <gregory.dylan.brown@gmail.com>
2018-09-18 21:40:38 -0500

    speechRecognition working (chrome and Ie

--- Lab1/js/imageSearch.js
+++ Lab1/js/imageSearch.js
@@ -1,5 +1,19 @@
 var imgApp = angular.module("imgApp", []);
 
-imgApp.controller('imgController', ['$scope', function($scope) {
 
-}]);
+
+imgApp.controller('imgController', function($scope, $http){
+    $scope.ygetImages = function () {
+        $http.get('http://search.yahooapis.com/ImageSearchService/V1/imageSearch?appid=mxA21b3c&query=' + $scope.searchTerm + '&results=1').success(function(data) {
+            console.log(data);
+        })
+    }
+
+    $scope.ggetImages = function () {
+        var searcher = new google.search.ImageSearch();
+        searcher.setRestriction(
+            google.search.ImageSearch.RESTRICT_COLORIZATION,
+            google.search.ImageSearch.COLORIZATION_GRAYSCALE
+        );
+    }
+});
--- Lab1/js/script.js
+++ Lab1/js/script.js
@@ -1,5 +1,3 @@
-
-
 function onSignIn(googleUser){
     var profile=googleUser.getBasicProfile();
     $(".g-signin2").css("display","none");
